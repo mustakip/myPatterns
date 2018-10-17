c6aff563868cf1430326f45c7494e9f87356a0b9
MUSTAKIM PATVEKAR <44019216+mustakip@users.noreply.github.com>
2018-10-17 10:43:54 +0530

    initial commit consist of three type of patterns

---
+++ diamondPattern/diamond.js
@@ -0,0 +1,114 @@
+const main = require("../diamond_functions_library.js").main;
+//const createStarMark = function(numberOfStars,width) {
+  //let line = "";
+  //let blankPosition = (width - numberOfStars)/2;
+  //for(let mark = 1; mark <=width; mark++) {
+    //let symbol = "*";
+    //if(mark <= blankPosition || mark > (width - blankPosition)) {
+      //symbol = " ";
+    //}
+    //line += symbol;
+  //}
+  //return line;
+//}
+//
+//const createHollowMark = function(numberOfStars,width) {
+  //let line = "";
+  //let markPosition = (width - numberOfStars)/2;
+  //for(let mark = 1; mark <= width; mark++) {
+    //let symbol = " ";
+    //if(mark == (markPosition+1) || mark == (width - markPosition)) {
+      //symbol = "*";
+    //}
+    //line += symbol;
+  //}
+  //return line;
+//}
+//
+//const createUpperSlash = function(numberOfStars,width) {
+  //let line = "";
+  //let markPosition = (width - numberOfStars)/2;
+  //for(let mark = 1; mark <= width; mark++) {
+    //let symbol = " ";
+    //if(mark == (markPosition+1) && numberOfStars != 1) {
+      //symbol = "/";
+    //}
+    //if(mark == (width-markPosition) && numberOfStars != width) {
+      //symbol = "\\";
+    //}
+    //line += symbol;
+  //}
+  //return line;
+//}
+//
+//const createlowerSlash = function(numberOfStars,width) {
+  //let line = "";
+  //let markPosition = (width - numberOfStars)/2;
+  //for(let mark = 1; mark <= width; mark++) {
+    //let symbol = " ";
+    //if(mark == (markPosition+1) && numberOfStars != 1) {
+      //symbol = "\\";
+    //}
+    //if(mark == width - markPosition) {
+      //symbol = "/";
+    //}
+    //line += symbol;
+  //}
+  //return line;  
+//}
+//
+//const createAngledDiamond = function(width) {
+  //let delimiter = "";
+  //let pattern = "";
+  //pattern += delimiter+createStarMark(1,width);
+  //delimiter = "\n";
+  //for(let numberOfStars = 3; numberOfStars < width; numberOfStars+= 2) {
+    //pattern += delimiter+createUpperSlash(numberOfStars,width);
+  //}
+  //pattern += delimiter+createHollowMark(width,width);
+  //for(let numberOfStars = width -2;numberOfStars > 1;numberOfStars-=2){
+    //pattern += delimiter+createlowerSlash(numberOfStars,width);
+  //}
+  //pattern += delimiter+createStarMark(1,width);
+  //return pattern;
+//}
+//
+//const createDiamond = function(width) {
+  //let delimiter = "";
+  //let pattern = "";
+  //let typeOfDiamond;
+  //for(let numberOfStars = 1; numberOfStars <=width; numberOfStars+= 2) {
+    //typeOfDiamond = createHollowMark(numberOfStars,width);
+    //if(process.argv[2] == "filled") {
+      //typeOfDiamond = createStarMark(numberOfStars,width);
+    //}
+    //pattern += delimiter+typeOfDiamond;
+    //delimiter = "\n";
+  //}
+  //for(let numberOfStars = width - 2;numberOfStars > 0;numberOfStars-=2){
+    //typeOfDiamond = createHollowMark(numberOfStars,width);
+    //if(process.argv[2] == "filled") {
+      //typeOfDiamond = createStarMark(numberOfStars,width);
+    //}
+    //pattern += delimiter+typeOfDiamond;
+  //}
+  //return pattern;
+//}
+//
+//const main = function() {
+  //let width = process.argv[3];
+  //let diamondType = process.argv[2];
+  //let output;
+//
+  //if(width % 2 ==0) {
+    //width -= 1;
+  //}
+  //if(diamondType == "hollow" || diamondType == "filled") {
+    //output = (createDiamond(width));
+  //}
+  //if(diamondType == "angled") {
+    //output = (createAngledDiamond(width));
+  //}
+  //console.log(output);
+//}
+main();
---
+++ rectanglePattern/rectanglePattern.js
@@ -0,0 +1,79 @@
+const generateEmptySymbol = function(width) {
+  let linePattern = "";
+  for(let marks = 1; marks <= (width); marks++) {
+    let symbol = " ";
+    if(marks == 1 || marks == width) {
+      symbol = "*";
+    }
+      linePattern = linePattern+symbol;
+    }
+  return linePattern;
+}
+
+const generateStarSymbol = function(width) {
+  let linePattern = "";
+  for(let marks = 1; marks <= width; marks++) {
+    linePattern = linePattern+"*";
+  }
+  return linePattern;
+}
+
+const generateDashSymbol = function(width) {
+  let linePattern = "";
+  for(let marks = 1; marks <= width; marks++) {
+    linePattern = linePattern+"-";
+  }
+  return linePattern;
+}
+
+const fill = function(width,height) {
+  let patterns = "";
+  let delimiter = "";
+  for(let line = 1; line <= height; line++) {
+    patterns = patterns+delimiter+generateStarSymbol(width)
+      delimiter = "\n";
+  }
+  return patterns;
+}
+
+const empty = function(width,height) {
+  let delimiter = "";
+  let pattern = "";
+  pattern = pattern+delimiter+generateStarSymbol(width);
+  delimiter = "\n";
+  for(let line = 1; line <= (height-2); line++) {
+    pattern = pattern+delimiter+generateEmptySymbol(width);
+  }
+  pattern = pattern+delimiter+generateStarSymbol(width);
+  return pattern;
+}
+
+const alternate = function(width,height) {
+  let pattern = "";
+  let delimiter = "";
+
+  for(let line = 1; line <= height; line++) {
+    let patterns = generateDashSymbol(width);
+    if(line % 2 != 0) {
+      patterns = generateStarSymbol(width)
+    }
+      pattern = pattern+delimiter+patterns;
+      delimiter = "\n";
+    }
+  return pattern;
+}
+let designType = process.argv[2];
+let width = process.argv[3];
+let height = process.argv[4];
+
+if(designType == "alternating") {
+  console.log(alternate(width,height));
+}
+
+if(designType == "empty") {
+  console.log(empty(width,height));
+}
+
+if(designType == "filled") {
+  console.log(fill(width,height));
+}
